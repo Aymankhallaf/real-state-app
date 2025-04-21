d5041bb2d9f2847ba1c91b6632afb25f6959b5d8
ayman khallaf <engayman.khalaf@gmail.com>
2025-04-21 18:16:34 +0200

    add css, js.. files to public folder

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route(path: '/', name: 'home')]
     /**
      * 
-     * @Route(   "/", name="home")
+     * @Route( "/", name="home")
      * @return Response
      */
     public function index(): Response
