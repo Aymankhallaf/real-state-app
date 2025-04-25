c1dd5756d2652aece69b4e76956fc4349b5e149b
ayman khallaf <engayman.khalaf@gmail.com>
2025-04-25 19:36:11 +0200

    add _ before marcos name

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PropertyRepository;
+
 
 final class PropertyController extends AbstractController
 {
     #[Route('/property', name: 'app_property')]
-    public function index(): Response
+    public function index(PropertyRepository $repository): Response
     {
+        // Fetch all properties from the database
+        $properties = $repository->findAll();
+        // Render the properties in a Twig template
         return $this->render('property/index.html.twig', [
             'controller_name' => 'PropertyController',
+            'properties' => $properties
         ]);
     }
 }
