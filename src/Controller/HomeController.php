88fa95d291180d70250f5515c3a704cae139ad9b
ayman khallaf <engayman.khalaf@gmail.com>
2025-05-04 17:08:16 +0200

    pass properties in home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PropertyRepository;
+use App\Entity\Property;
 
 final class HomeController extends AbstractController
 {
@@ -14,10 +16,13 @@ final class HomeController extends AbstractController
      * @Route( "/", name="home")
      * @return Response
      */
-    public function index(): Response
+    public function index(PropertyRepository $repository): Response
     {
+        $properties = $repository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'properties' => $properties
+
         ]);
     }
 }
