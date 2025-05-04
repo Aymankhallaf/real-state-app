b24824a24e70e61126b5ba5e4f7134ef54a5eb30
ayman khallaf <engayman.khalaf@gmail.com>
2025-05-04 23:40:27 +0200

    create Property form

---
+++ src/Form/PropertyTypeForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyTypeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('price')
+            ->add('address')
+            ->add('createdAt')
+            ->add('room')
+            ->add('bed')
+            ->add('area')
+            ->add('type')
+            ->add('purpose')
+            ->add('bathroom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+        ]);
+    }
+}
