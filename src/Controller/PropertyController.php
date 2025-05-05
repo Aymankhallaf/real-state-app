61dd97bcf9cb681e7f7b8bfa120720ccc43f30f3
ayman khallaf <engayman.khalaf@gmail.com>
2025-05-05 10:35:29 +0200

    save property in database ok

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\PropertyRepository;
 use App\Form\PropertyForm;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Property;
 
 final class PropertyController extends AbstractController
 {
@@ -35,9 +39,20 @@ final class PropertyController extends AbstractController
     }
 
     #[Route('/properties/create', name: 'property_create')]
-    public function createProperty(): Response
+    public function createProperty(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $productForm = $this->createForm(PropertyForm::class);
+        $property = new Property();
+        $productForm = $this->createForm(PropertyForm::class, $property);
+        $productForm->handleRequest($request);
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            // Handle the form submission and save the property to the database
+            $property = $productForm->getData();
+            $entityManager->persist($property);
+            $entityManager->flush();
+
+            // Redirect to the property list page after successful creation
+            return $this->redirectToRoute('properties');
+        }
         // Render the property creation form in a Twig template
         return $this->render('property/create.html.twig', [
             'controller_name' => 'PropertyController',
