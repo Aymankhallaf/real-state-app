b3b1f4f1df6bc008dc244fc7814f9ff784f1d5ca
ayman khallaf <engayman.khalaf@gmail.com>
2025-05-13 22:06:08 +0200

    remove uploadedAt and show imge (true)

--- src/Form/ImageForm.php
+++ src/Form/ImageForm.php
@@ -1,29 +1,31 @@
 <?php
-
+// src/Form/ImageType.php
 namespace App\Form;
 
 use App\Entity\Image;
-use App\Entity\Property;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ImageForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('imageFile', VichImageType::class, [
-            'label' => 'Upload Image',
-            'required' => false,
-            'allow_delete' => true,
-            'download_uri' => true,
-        ])
-            ->add('alt')
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
-            ]);
+        $builder
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'label' => 'Upload Image',
+                'download_uri' => false,
+                'image_uri' => true
+            ])
+            ->add('alt', TextType::class, [
+                'required' => false,
+                'label' => 'Alt text',
+            ])
+       ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
