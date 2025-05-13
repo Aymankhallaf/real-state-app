908ece9334960a2c86c0812f3cec32b44b1c7a23
ayman khallaf <engayman.khalaf@gmail.com>
2025-05-13 10:41:23 +0200

    create imageForm

---
+++ src/Form/ImageForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use App\Entity\Property;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImageForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('url')
+            ->add('alt')
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('property', EntityType::class, [
+                'class' => Property::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
