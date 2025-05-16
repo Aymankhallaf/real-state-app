1aa01af4df95ff4def5788977f5cda81d49eb8ad
ayman khallaf <engayman.khalaf@gmail.com>
2025-05-16 10:23:16 +0200

    modify type to entity PropertyType

--- src/Model/PropertySearch.php
+++ src/Model/PropertySearch.php
@@ -1,35 +1,38 @@
 <?php
+
 namespace App;
+
+use App\Entity\PropertyType;
 
 class PropertySearch
 {
-private ?string $title = null;
-private ?string $purpose = null;
-private ?int $type = null;
+    private ?string $title = null;
+    private ?string $purpose = null;
+    private ?PropertyType  $type = null;
 
-public function getTitle(): ?string
-{
-return $this->title;
-}
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
 
-public function setTitle(?string $title): void
-{
-$this->title = $title;
-}
-public function getPurpose(): ?string
-{
-return $this->purpose;
-}
-public function setPurpose(?string $purpose): void
-{
-$this->purpose = $purpose;
-}
-public function getType(): ?int
-{
-return $this->type;
-}
-public function setType(?int $type): void
-{
-$this->type = $type;
-}
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+    public function getPurpose(): ?string
+    {
+        return $this->purpose;
+    }
+    public function setPurpose(?string $purpose): void
+    {
+        $this->purpose = $purpose;
+    }
+    public function getType(): ?PropertyType
+    {
+        return $this->type;
+    }
+    public function setType(PropertyType $type): void
+    {
+        $this->type = $type;
+    }
 }
