399297de72d97b6e024048ca80dc4449b3b0d1be
ayman khallaf <engayman.khalaf@gmail.com>
2025-05-19 17:11:44 +0200

    showMenuTypes()

--- src/Controller/PropertyTypeController.php
+++ src/Controller/PropertyTypeController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\PropertyTypeRepository;
-use App\Entity\PropertyType;
 
 final class PropertyTypeController extends AbstractController
 {
@@ -22,17 +21,27 @@ final class PropertyTypeController extends AbstractController
     }
 
     #[Route('/types/{name}', name: 'property_type_show')]
-    public function showPropertiesByType(string $name, PropertyTypeRepository $repository  ): Response
+    public function showPropertiesByType(string $name, PropertyTypeRepository $repository): Response
     {
-        $type =$repository->findOneByName($name);
-    
-    
+        $type = $repository->findOneByName($name);
+
+
         $properties = $type->getProperties();
         // Render the property type details in a Twig template
         return $this->render('property_type/showType.html.twig', [
             'controller_name' => 'PropertyTypeController',
             'type' => $type,
             'properties' => $properties
+        ]);
+    }
+
+    #[Route(name: 'type_menu')]
+    public function showMenuTypes(PropertyTypeRepository $repository): Response
+    {
+        $types = $repository->findAll();
+        return $this->render('includes/navbar.html.twig', [
+            'controller_name' => 'PropertyTypeController',
+            'types' => $types
         ]);
     }
 }
