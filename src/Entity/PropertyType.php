b2d63d78387efc6d1eac38dfd634817f26c2cac5
ayman khallaf <engayman.khalaf@gmail.com>
2025-05-19 13:28:52 +0200

    join ORM colum in one line

--- src/Entity/PropertyType.php
+++ src/Entity/PropertyType.php
@@ -17,8 +17,7 @@ class PropertyType
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 15)]
-    #[ORM\Column(unique: true)]
+    #[ORM\Column(length: 15,unique: true)]
     private ?string $name = null;
 
     #[ORM\Column(length: 255, nullable: true)]
